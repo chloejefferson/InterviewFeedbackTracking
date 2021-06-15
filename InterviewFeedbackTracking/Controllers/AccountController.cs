5389faffcd8e8e5e41f2c852d26c598f3f742e58
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-06-15 13:04:10 -0400

    updated connection string and tested registering user

--- InterviewFeedbackTracking/Controllers/AccountController.cs
+++ InterviewFeedbackTracking/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using InterviewFeedbackTracking.Models;
+using InterviewFeedbackTracking.Data;
 
 namespace InterviewFeedbackTracking.Controllers
 {
