46945741d68933dde3a2f35cd11ecb3038ca9ae2
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-06-15 12:22:11 -0400

    Add project files.

---
+++ InterviewFeedbackTracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterviewFeedbackTracking.Startup))]
+namespace InterviewFeedbackTracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
