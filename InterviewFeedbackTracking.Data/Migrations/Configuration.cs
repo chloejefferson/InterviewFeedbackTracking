96c75ed8d45b2d109768dd62449f802866db0aa8
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-06-15 13:10:06 -0400

    updated data to include nullable datetimes (for modifieddate)

---
+++ InterviewFeedbackTracking.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace InterviewFeedbackTracking.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<InterviewFeedbackTracking.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "InterviewFeedbackTracking.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(InterviewFeedbackTracking.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
