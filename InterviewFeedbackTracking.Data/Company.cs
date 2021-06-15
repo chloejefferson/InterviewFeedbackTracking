e74926e4754871f241e3d31b8102ab1eac0a58b5
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-06-15 12:53:02 -0400

    interview and company data

---
+++ InterviewFeedbackTracking.Data/Company.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewFeedbackTracking.Data
+{
+    public enum CompanyType
+    {
+        Tech = 1,
+        Financial,
+        Healthcare,
+        Agriculture,
+        Construction,
+        Other
+    }
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Website { get; set; }
+
+        public CompanyType TypeOfCompany { get; set; }
+
+        public virtual List<Interview> Interviews { get; set; } = new List<Interview>();
+    }
+}
---
+++ InterviewFeedbackTracking.Data/Interview.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewFeedbackTracking.Data
+{
+    public enum InterviewType
+    {
+        PhoneScreen = 1,
+        TechScreen,
+        CultureScreen,
+        Other
+    }
+    public class Interview
+    {
+        [Key]
+        public int InterviewId { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public int CompanyId { get; set; }
+
+        public virtual Company Company { get; set; }
+
+        public string Interviewer { get; set; }
+
+        public Guid Interviewee { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime DateOfInterview { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public InterviewType TypeOfInterview { get; set; }
+
+        public string Comment { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
