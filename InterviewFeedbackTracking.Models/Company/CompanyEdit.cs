69b63bfb9a1bdc4cfe96a43584c821e6dfe86198
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-06-15 21:15:33 -0400

    company models and service built out

---
+++ InterviewFeedbackTracking.Models/Company/CompanyEdit.cs
@@ -0,0 +1,39 @@
+﻿using InterviewFeedbackTracking.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewFeedbackTracking.Models.Company
+{
+    public class CompanyEdit
+    {
+        [Required]
+        public int CompanyId { get; set; }
+
+        //[Required]
+        public string Name { get; set; }
+
+        //[Required]
+        public string StreetAddress { get; set; }
+
+        //[Required]
+        public string City { get; set; }
+
+        //[Required]
+        public string State { get; set; }
+
+        //[Required]
+        public string Zip { get; set; }
+
+        //[Required]
+        public string Website { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        //[Required]
+        public IndustryEnum Industry { get; set; }
+    }
+}
